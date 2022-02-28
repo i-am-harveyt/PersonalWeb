863cf76597d214badacd2b98bb6e56ea948c24e3
Hao-Ting Tung <haotingtong30@gmail.com>
2022-02-28 10:59:42 +0800

    make post-like block, adjust size and margin props

--- src/Components/ContentBlock/ContentBlock.js
+++ src/Components/ContentBlock/ContentBlock.js
@@ -1,3 +1,15 @@
-import react from "react";
-import reactDom from "react-dom";
-import "./ContentBlock.css"
+import "./ContentBlock.css";
+import image from "../../Images/cat.jpg";
+
+export default function ContentBlock() {
+  return (
+    <div className="block">
+      <div className="head-bar"></div>
+      <img src={image} alt="" className="image-in-block" />
+      <div className="function-bar"></div>
+      <div className="word-block">
+        <p className="words">Cute Cute Kitty!</p>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -1,12 +1,15 @@
 import React from "react";
-import "../Styles/Content.css"
+import ContentBlock from "../Components/ContentBlock/ContentBlock"
+import "../Components/ContentBlock/ContentBlock.css"
 
 export default function Work() {
   return (
-    <div className="body">
-      <div className="content">
-        <h1>Work</h1>
-      </div>
+    <div>
+      <ContentBlock/>
+      <ContentBlock/>
+      <ContentBlock/>
+      <ContentBlock/>
+      <ContentBlock/>
     </div>
   );
 }
