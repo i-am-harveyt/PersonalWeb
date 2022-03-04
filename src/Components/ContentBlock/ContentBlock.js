c52b44f10be3290e854b7314a1a83b3e13286882
Hao-Ting Tung <haotingtong30@gmail.com>
2022-03-05 00:17:17 +0800

    Minimal set headbar font margi, variablize colors

--- src/Components/ContentBlock/ContentBlock.js
+++ src/Components/ContentBlock/ContentBlock.js
@@ -4,7 +4,9 @@ import image from "../../Images/cat.jpg";
 export default function ContentBlock() {
   return (
     <div className="block">
-      <div className="head-bar"></div>
+      <div className="head-bar">
+        <h2 className="block-title">Block Title</h2>
+      </div>
       <img src={image} alt="" className="image-in-block" />
       <div className="function-bar"></div>
       <div className="word-block">
