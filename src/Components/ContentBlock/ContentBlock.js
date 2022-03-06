72ae22843039f0daee1871d23d96798e7e15900d
Hao-Ting Tung <haotingtong30@gmail.com>
2022-03-06 11:08:59 +0800

    Edit the layout of the Work page

--- src/Components/ContentBlock/ContentBlock.js
+++ src/Components/ContentBlock/ContentBlock.js
@@ -1,16 +1,16 @@
 import "./ContentBlock.css";
 import image from "../../Images/cat.jpg";
 
-export default function ContentBlock() {
+export default function ContentBlock(props) {
   return (
     <div className="block">
       <div className="head-bar">
-        <h2 className="block-title">Block Title</h2>
+        <h2 className="block-title">{props.title}</h2>
       </div>
       <img src={image} alt="" className="image-in-block" />
       <div className="function-bar"></div>
       <div className="word-block">
-        <p className="words">Cute Cute Kitty!</p>
+        <p className="words">{props.content}</p>
       </div>
     </div>
   );
--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "../Styles/Work.css"
 import ContentBlock from "../Components/ContentBlock/ContentBlock"
 import "../Components/ContentBlock/ContentBlock.css"
 
 export default function Work() {
   return (
-    <div>
-      <ContentBlock/>
+    <div className="works">
+      <div className="Blocks">
+      <ContentBlock title="1. Personal Website"/>
+      <ContentBlock title="2. GenoMaker"/>
+      <ContentBlock title="3. Tiny Shooting"/>
+      <ContentBlock title='4. '/>
+      <ContentBlock title='5. '/>
+      <ContentBlock title='6. '/>
+      <ContentBlock title='7. '/>
+      </div>
     </div>
   );
 }
