864f9d7c394bb6270b3f75b42c2855f825085ed0
Hao-Ting Tung <haotingtong30@gmail.com>
2022-03-07 10:53:04 +0800

    WorkPage and ContentBlock layout edit

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -6,7 +6,7 @@ import "../Components/ContentBlock/ContentBlock.css"
 export default function Work() {
   return (
     <div className="works">
-      <div className="Blocks">
+      <div className="blocks">
       <ContentBlock title="1. Personal Website"/>
       <ContentBlock title="2. GenoMaker"/>
       <ContentBlock title="3. Tiny Shooting"/>
